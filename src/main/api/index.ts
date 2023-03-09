9835103441264195d5f4fe7f8b52685c04306007
ronan <ronanportela_@hotmail.com>
2023-03-09 19:09:45 -0300

    add husky

--- src/main/api/index.ts
+++ src/main/api/index.ts
@@ -1,6 +1,6 @@
-import '../config/module-alias';
-import { PersonController } from "@/application/controllers";
+import '../config/module-alias'
+import { PersonController } from '@/application/controllers'
 
-const p = new PersonController();
-console.log(p.speak('ronan'));
-console.log(p.speak());
+const p = new PersonController()
+console.log(p.speak('ronan'))
+console.log(p.speak())
